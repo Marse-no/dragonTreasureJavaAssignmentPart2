aa7653be36741e49850206751311864edb1abd16
Marianne <marianne.selnes@gmail.com>
2024-12-09 14:27:12 +0100

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        // create and initialize dungeon object
-        Dungeon dungeon = new Dungeon();
-        // start game
-        dungeon.playGame();
-    }
-}
